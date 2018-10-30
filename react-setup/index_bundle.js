0cdc2270a85c3e5c5121fb5dc3b9308ec419c333
saefulrizky <saefulrizky2@gmail>
2018-10-30 17:45:08 +0700

    add main App and index_bundle

---
+++ react-setup/index_bundle.js
@@ -0,0 +1,11 @@
+<!DOCTYPE hyml>
+<html lang = "en">
+  <head>
+    <meta charset = "UTD-8">
+    <title>React App</title>
+  </head>
+  <body>
+    <div id = "app"></div>
+    <script src = 'index_bundle.js'></script>
+  </body>
+</html>
