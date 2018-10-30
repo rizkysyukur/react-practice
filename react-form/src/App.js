b30709dee29a716867a31e478a4fdcbcd4b24c9a
saefulrizky <saefulrizky2@gmail>
2018-10-30 11:09:52 +0700

    add App and main

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: 'Initial data...'
+    }
+    this.updateState = this.updateState.bind(this);
+  };
+  updateState(e){
+    this.setState({data: e.target.value});
+  }
+  render(){
+    return(
+      <div>
+        <input type = "text" value = {this.state.data}
+        onChange = {this.updateState} />
+        <h4>{this.state.data}</h4>
       </div>
     );
   }
 }
-
 export default App;
