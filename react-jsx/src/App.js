2be9ec72e6b537500dd850779b22bf1a5098b890
saefulrizky <saefulrizky2@gmail>
2018-10-31 22:34:05 +0700

    add react-jsx'

---
+++ react-jsx/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>Header</h1>
+        {/*Multi line comment...*/}
+      </div>
+    );
+  }
+}
+
+export default App;
